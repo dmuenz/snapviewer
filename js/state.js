0ee6b13290fdb64d5693bb0dc1804b3283de0a4d
dmuenz <dmuenz@gmail.com>
2026-06-13 00:14:44 -0700

    Update state.js to track temporary session status

--- js/state.js
+++ js/state.js
@@ -14,5 +14,6 @@ export const state = {
   currentZoom: 'zoom-fit-width',
   currentMedia: null,
   treeGeneration: 0,
-  openPaths: new Set()
+  openPaths: new Set(),
+  isTemporarySession: false // Track if current session is webkitdirectory (temporary)
 };
