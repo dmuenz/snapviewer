6397f6a8599d580501748f79ab9a279ddb58ef56
dmuenz1 <dmuenz1@its.jnj.com>
2026-07-10 14:08:42 -0700

    Refactor codebase.R to create files for uploading instead of copy & pasting

---
+++ tools/codebase2.R
@@ -0,0 +1,133 @@
+# Print the directory tree and file contents for all 1st-party code files
+# for the SnapViewer project. The output is chunked and saved into text files,
+# each of at most 50,000 characters.
+# -Daniel Muenz
+
+this_dir <- this.path::this.dir()
+
+# move to project root
+setwd(this.path::this.proj())
+proj <- basename(getwd())
+
+cat_glue <- function(..., .env = parent.frame()) {
+  cat(glue::glue(..., .sep = "\n", .envir = .env), "\n", sep = "")
+}
+
+# chunk a character vector into strings whose total width stays under max_char
+chunk_by_nchar <- function(x, max_char, sep = "") {
+  stopifnot(is.character(x), length(max_char) == 1, max_char >= 1)
+
+  y <- character()
+  current <- ""
+
+  for (s in x) {
+    # candidate if we append s to current chunk
+    candidate <- if (current == "") s else paste0(current, sep, s)
+
+    if (nchar(candidate) < max_char) {
+      current <- candidate
+    } else {
+      # flush current chunk (if non-empty)
+      if (current != "") y <- c(y, current)
+
+      # start new chunk with s (even if s itself is too long)
+      current <- s
+    }
+  }
+
+  # flush final chunk
+  if (current != "") y <- c(y, current)
+
+  y
+}
+
+intro <- capture.output({
+  # intro text mentioning currently checked out git branch
+  cat_glue("
+Here is a copy, chunked into NUM_CHUNKS .txt files, of all the 1st-party code \\
+for a static web app called SnapViewer.
+* The app is a tool to help R package developers easily view their unit \\
+testing snapshot files created by testthat.
+* The app is hosted on GitHub Pages.
+* Specifically the code shown here is for the `{gert::git_branch()}` git branch.
+
+Here is a tree-like view of the {proj} project folder, showing names of all \\
+files and folders:
+
+```text")
+
+  # print directory tree, including only relevant files
+  withr::with_dir("..", {
+    fs::dir_tree(path = proj, regexp = "^(?!snapviewer/tools/).+\\..+$",
+                 perl = TRUE)
+  })
+
+  cat_glue("```
+
+Below, in fenced code blocks, are the contents of all the 1st-party code \\
+(HTML/CSS/JS) and markdown files.
+
+The code is chunked into NUM_CHUNKS txt files for easier uploading to you. \\
+Currently you are reading `OUTNAME1`. \\
+After you finish reading this file, read the following additional files to \\
+understand the entire codebase:
+
+OUTNAME2TON_LIST
+
+Code in the `vendor` folder, from 3rd-party vendors, is not shown, \\
+but the `vendor/README.md` file describes the origin and purpose of this code.")
+}) |> paste(collapse = "\n")
+
+# all 1st-party code files, with top-level files first
+files <- fs::dir_ls(regexp = "\\.(html|css|js|md)$", recurse = TRUE)
+files <- files[!grepl("^vendor/.*/", files)]
+files <- files[order(grepl("/", files, fixed = TRUE))]
+
+# get file contents in long strings
+file_contents <- sapply(files, \(file) {
+  capture.output({
+    cat_glue("
+
+Here is the file `{file}`:
+```{fs::path_ext(file)}")
+    cat(readLines(file), sep = "\n")
+    cat_glue("```")
+  }) |> paste(collapse = "\n")
+})
+
+chunks <- chunk_by_nchar(
+  c(intro, file_contents, "\nOK, that's the complete 1st-party codebase."),
+  max_char = 50000,
+  sep = "\n"
+)
+
+out_fnames <- sprintf("codebase_%02d.txt", seq_along(chunks))
+
+num_chunks <- length(chunks)
+
+chunks[-num_chunks] <- paste0(
+  chunks[-num_chunks],
+  "\n\nEnd of `", out_fnames[-num_chunks], "`. Next read `", out_fnames[-1], "`."
+)
+
+chunks[1] <- gsub("NUM_CHUNKS", num_chunks, chunks[1])
+chunks[1] <- gsub("OUTNAME1", out_fnames[1], chunks[1])
+chunks[1] <- gsub("OUTNAME2TON_LIST",
+                  paste0('* `', out_fnames[-1], '`', collapse = '\n'),
+                  chunks[1])
+
+cat_glue("There will be {length(chunks)} files with the following lengths:
+         {paste(nchar(chunks), collapse = ', ')}")
+
+withr::with_dir(this_dir, {
+  old_files <- list.files(pattern = "^codebase_\\d+\\.txt$")
+  file.remove(old_files)
+
+  for (i in seq_along(chunks)) {
+    writeLines(
+      text     = enc2utf8(chunks[i]),
+      con      = out_fnames[i],
+      useBytes = TRUE
+    )
+  }
+})
